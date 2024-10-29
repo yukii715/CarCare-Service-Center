5406e77b4a142168fa912a6e70dcecb091fb8376
TI QI XIANG <TP081020@mail.apu.edu.my>
2024-10-29 12:40:12 +0800

    29/10

---
+++ CarCare Service Center/AdminMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Appearance;
+
+namespace CarCare_Service_Center
+{
+    public partial class frmAdminMain : Form
+    {
+        public frmAdminMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAdminMain_Load(object sender, EventArgs e)
+        {
+            tabAdmin.DrawItem += Draw_Item.tabControlAdjustment;
+        }
+
+        private void btnStaffSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSortStaffID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSortStaffName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStaffDetail_Click(object sender, EventArgs e)
+        {
+            StaffDetails formStaffDetails = new StaffDetails();
+            formStaffDetails.Show();
+            
+        }
+
+        private void btnStaffDelete_Click(object sender, EventArgs e)
+        {
+            StaffDeletion formStallDeletion = new StaffDeletion();
+            formStallDeletion.Show();
+        }
+
+        private void btnStaffAdd_Click(object sender, EventArgs e)
+        {
+            StaffInsertion formStaffInsertion = new StaffInsertion();
+            formStaffInsertion.Show();
+        }
+
+        private void btnServiceSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnServiceSortID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnServiceSortName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnServiceSortServiceType_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnServiceDetails_Click(object sender, EventArgs e)
+        {
+            ServiceDetails formServiceDetails = new ServiceDetails();
+            formServiceDetails.Show();
+        }
+    }
+}
---
+++ CarCare Service Center/StaffDetails.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CarCare_Service_Center
+{
+    partial class StaffDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblStaffSalary = new System.Windows.Forms.Label();
+            this.lblStaffRole = new System.Windows.Forms.Label();
+            this.lblStaffEmail = new System.Windows.Forms.Label();
+            this.lblStaffName = new System.Windows.Forms.Label();
+            this.lblStaffID = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Staff ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(100, 453);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 40);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Role:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(100, 573);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 40);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Salary: ";
+            // 
+            // lblStaffSalary
+            // 
+            this.lblStaffSalary.AutoSize = true;
+            this.lblStaffSalary.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffSalary.Location = new System.Drawing.Point(332, 573);
+            this.lblStaffSalary.Name = "lblStaffSalary";
+            this.lblStaffSalary.Size = new System.Drawing.Size(28, 40);
+            this.lblStaffSalary.TabIndex = 9;
+            this.lblStaffSalary.Text = "-";
+            // 
+            // lblStaffRole
+            // 
+            this.lblStaffRole.AutoSize = true;
+            this.lblStaffRole.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffRole.Location = new System.Drawing.Point(332, 453);
+            this.lblStaffRole.Name = "lblStaffRole";
+            this.lblStaffRole.Size = new System.Drawing.Size(28, 40);
+            this.lblStaffRole.TabIndex = 8;
+            this.lblStaffRole.Text = "-";
+            // 
+            // lblStaffEmail
+            // 
+            this.lblStaffEmail.AutoSize = true;
+            this.lblStaffEmail.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffEmail.Location = new System.Drawing.Point(332, 333);
+            this.lblStaffEmail.Name = "lblStaffEmail";
+            this.lblStaffEmail.Size = new System.Drawing.Size(28, 40);
+            this.lblStaffEmail.TabIndex = 7;
+            this.lblStaffEmail.Text = "-";
+            // 
+            // lblStaffName
+            // 
+            this.lblStaffName.AutoSize = true;
+            this.lblStaffName.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffName.Location = new System.Drawing.Point(332, 213);
+            this.lblStaffName.Name = "lblStaffName";
+            this.lblStaffName.Size = new System.Drawing.Size(28, 40);
+            this.lblStaffName.TabIndex = 6;
+            this.lblStaffName.Text = "-";
+            // 
+            // lblStaffID
+            // 
+            this.lblStaffID.AutoSize = true;
+            this.lblStaffID.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffID.Location = new System.Drawing.Point(332, 93);
+            this.lblStaffID.Name = "lblStaffID";
+            this.lblStaffID.Size = new System.Drawing.Size(28, 40);
+            this.lblStaffID.TabIndex = 5;
+            this.lblStaffID.Text = "-";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(48, 709);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(140, 57);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(338, 709);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 57);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Comic Sans MS", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(628, 709);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(140, 57);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // StaffDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 808);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lblStaffSalary);
+            this.Controls.Add(this.lblStaffRole);
+            this.Controls.Add(this.lblStaffEmail);
+            this.Controls.Add(this.lblStaffName);
+            this.Controls.Add(this.lblStaffID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StaffDetails";
+            this.Text = "StaffDetails";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblStaffSalary;
+        private System.Windows.Forms.Label lblStaffRole;
+        private System.Windows.Forms.Label lblStaffEmail;
+        private System.Windows.Forms.Label lblStaffName;
+        private System.Windows.Forms.Label lblStaffID;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
